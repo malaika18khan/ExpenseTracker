668029a651c3b1fc2e5d06ba8636c9fcb62a00ca
Malaika Khan <malaika18khan@gmail.com>
2024-02-26 12:56:59 +0500

    UI Building

---
+++ app/src/main/java/com/malaika18khan/expensetracker/RegisterUserActivity.kt
@@ -0,0 +1,11 @@
+package com.malaika18khan.expensetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
